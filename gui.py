9ad03c6d6c6947c961cb43f9543cd1078c029778
n1tr06 <eitangil1016@gmail.com>
2024-07-03 10:39:24 +0300

    draft

---
+++ gui.py
@@ -0,0 +1,49 @@
+import tkinter as tk
+from tkinter import filedialog
+import main 
+import time
+
+def browse_folder():
+    global directory
+    directory = filedialog.askdirectory()
+
+    if directory:
+        
+        folder_path_label.config(text=f"Selected Folder: {directory}", font = font)
+        scan_button.pack(pady=10)
+
+def scan_files():
+    scan_button.config(text = "Scanning...")
+    
+
+    file_listbox.pack(side=tk.TOP, fill=tk.Y)
+    scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+    file_listbox.config(yscrollcommand=scrollbar.set)
+    scrollbar.config(command=file_listbox.yview)
+
+    lis = main.all_paths(directory)
+    for i in lis:
+        file_listbox.insert(tk.END, i)
+        
+    scan_button.config(text = "Scan Complete")
+
+window = tk.Tk()
+
+window.title("Anti Virus")
+window.geometry("300x400")
+
+font = ("Arial", 14)
+
+folder_path_label = tk.Label(text="No folder selected yet.", font = font)
+folder_path_label.pack()
+
+browse_button = tk.Button(text="Browse Folder", command=browse_folder, font=font)
+browse_button.pack(pady=10)
+
+file_listbox = tk.Listbox(window)
+scrollbar = tk.Scrollbar(window, orient=tk.VERTICAL)
+
+scan_button = tk.Button(text="Start Scan",command =scan_files  ,font = font)
+
+window.mainloop()
---
+++ main.py
@@ -0,0 +1,55 @@
+import os
+import requests
+
+def all_paths(path):
+
+    path_list = []
+    for root, _, files in os.walk(path):
+        for filename in files:
+            path_list.append(os.path.join(root, "\\", filename))
+    return path_list
+
+def submit_to_virustotal(api_key, path):
+
+    with open(path, "rb") as f:
+
+        files = {'file': f}
+        url = "https://www.virustotal.com/api/v3/files"
+        headers = {"x-apikey": api_key}
+        response = requests.post(url, files=files , headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['id']
+        else:
+            print(f"Error submitting file: {response.text}")
+            return None
+        
+def get_hash_from_analyses(id):
+    
+    url = f'https://www.virustotal.com/api/v3/analyses/{id}'
+    headers = {"x-apikey": api_key}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.json()['meta']['file_info']['sha256']
+    else:
+        print(f"Error getting analysis status: {response.text}")
+        return None
+
+def get_file_report(api_key, hash):
+
+    url = f"https://www.virustotal.com/api/v3/files/{hash}"
+    headers = {"x-apikey": api_key}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.json()['data']['attributes']['last_analysis_stats']
+    else:
+        print(f"Error getting scan results: {response.text}")
+        return None
+
+api_key = '6c98224bedab17e010a3a89f9bcbf0a28292b4671c81a92151195d9037f6a2be'
+path = r'D:\\!objectcheck\\app.py'
+
+id = submit_to_virustotal(api_key, path)
+hash = get_hash_from_analyses(id)
+
+print(get_file_report(api_key, hash))
